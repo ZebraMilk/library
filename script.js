24d260a7bd7eb53ef4f2a8926fbdfc55afd511c1
ZebraMilk <zebramilkx@gmail.com>
2022-03-05 19:51:00 -0600

    Add pseudocode to .js file

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-let myLibrary = [];
-let arrayIndex = 0;
-
-const display = document.querySelector(".display");
-const submit = document.querySelector("#submit");
-const form = document.querySelector(".form-fields");
-
-submit.addEventListener("click", getBookInfo());
-
-
-
-
-
+const myLibrary = [];
+let arrayLength = 0;
 
 function Book(title, author, published, pages, isRead) {
   this.title = title;
@@ -25,6 +14,81 @@ Book.prototype = {
     return `${this.title} by ${this.author}, ${this.pages} pages, published in ${this.published}, ${this.isRead ? "read" : "not read yet"}.`
   }
 };
+
+
+const display = document.querySelector(".display");
+const submit = document.querySelector("#submit");
+const form = document.querySelector(".form-fields");
+
+submit.addEventListener("click", addBook());
+
+
+// 1. User enters data into the form
+//   - Validate published and pages if they include it.
+//   - Mark the required fields with an asterisk
+
+
+
+// 2. Button click
+//     - validate there is a Title and an Author
+//     - passes the form data into a function
+//     - clears the forms
+
+// 3. take the data from the forms
+//     let newBook
+//     For each field
+//         store user input in a variable
+//         assign the variable to the corresponding newBook property
+//     add newBook to library
+function getBookInfo() {
+  let title1 = document.getElementById("title").value;
+  let author1 = document.getElementById("author").value;
+  let published1 = document.getElementById("published").value;
+  let pages1 = document.getElementById("pages").value;
+  let isRead1 = document.getElementById("isRead").value;
+  
+  // let bookTitle = toCamelCase(title);
+  let newBook = new Book(`${title1}`, `${author1}`, `${published1}`, `${pages1}`, `${isRead1}`);
+  console.table(newBook);
+  return newBook;
+}
+
+// 4. addBook
+function addBook() {
+  addBookToLibrary(getBookInfo());
+  console.table(myLibrary);
+}
+//     if no books in Library
+//         library[0] is newBook
+//     at myLibrary[length] (?)
+//     or just concat
+//         newBook
+
+// 5. make new card(takes a Book as an argument)
+//     - make a div with
+//         - 4 containers
+//         - auto-columns for the buttons I will add
+//             - delete button (removes book)
+//             - isRead button (red or green depending)
+//     - set the innerText of each column to the approrpiate property
+
+// 6. remove book
+//     - slice/splice the array before and after the button clicked
+//     - remove the div containing that book
+//     - update the attributes on each remaining book-card/row to reflect the new size of the myLibrary array
+
+// 7. clear forms
+//     - set the input fields back to their defaults
+// 8. updateLibrary
+
+//     updateLibraryDisplay
+
+
+
+//     bonus: Move the cards around, reorder the list/table/cards
+
+
+
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 1937, 295, true);
 const ulysses = new Book("Ulysses", "James Joyce", 1922, 730, true);
@@ -41,20 +105,6 @@ const bloodBonesButter = new Book("Blood, Bones & Butter", "Gabrielle Hamilton",
 // });
 
 
-
-function getBookInfo() {
-  let title = document.getElementById("title").value;
-  let author = document.getElementById("author").value;
-  let published = document.getElementById("published").value;
-  let pages = document.getElementById("pages").value;
-  let isRead = document.getElementById("isRead").value;
-  
-  // let bookTitle = toCamelCase(title);
-  let newBook = new Book(title, author, published, pages, isRead)
-  console.table(newBook);
-  addBookToLibrary(newBook);
-  console.table(myLibrary);
-}
 
 function addBookToLibrary(book) {
   if(!myLibrary[0]) {
